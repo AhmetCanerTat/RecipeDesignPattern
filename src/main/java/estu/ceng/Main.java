4f9fc966a73b580daa73b82f6d2b9c944af349b2
Ahmet Caner Tat <caner-tat@hotmail.com>
2024-05-04 20:54:33 +0300

    First Commit

---
+++ src/main/java/estu/ceng/Main.java
@@ -0,0 +1,13 @@
+package estu.ceng;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
